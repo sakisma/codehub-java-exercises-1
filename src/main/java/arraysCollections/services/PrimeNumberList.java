64c7bb42626fce862ba98a977c1bcec0ce246d4e
Alexandros Kalaitzidis <alexkalaitzidis2@gmail.com>
2020-08-29 00:59:54 +0300

    Fix prime number logic mistake

--- src/main/java/arraysCollections/services/PrimeNumberList.java
+++ src/main/java/arraysCollections/services/PrimeNumberList.java
@@ -13,7 +13,7 @@ public class PrimeNumberList {
 
         for(int i = 0; i < 25; i++) {
             listAllIntegers.add(i);
-            if (i % 2 == 0)
+            if (i % 2 != 0)
                 primeNumberList.add(i);
         }
         return "The starting list is " + listAllIntegers + "\nand the prime number list is " + primeNumberList;
