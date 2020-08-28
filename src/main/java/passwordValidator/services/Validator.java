79735d1579365ca35255b26ce72df08c47f0022b
Alexandros Kalaitzidis <alexkalaitzidis2@gmail.com>
2020-08-29 00:38:21 +0300

    Simplify according to IDE

--- src/main/java/passwordValidator/services/Validator.java
+++ src/main/java/passwordValidator/services/Validator.java
@@ -3,51 +3,35 @@ package passwordValidator.services;
 import java.util.ArrayList;
 
 public class Validator {
+    public Validator() {
+    }
 
     public boolean hasOneUppercase(String inputString) {
-        if (!inputString.equals(inputString.toUpperCase()))
-            return true;
-        else
-            return false;
+        return !inputString.equals(inputString.toUpperCase());
     }
 
     public boolean hasOneLowercase(String inputString) {
-        if (!inputString.equals(inputString.toLowerCase()))
-            return true;
-        else
-            return false;
+        return !inputString.equals(inputString.toLowerCase());
     }
 
     public boolean hasOneNumber(String inputString) {
-        if (inputString.matches(".*\\d.*"))
-            return true;
-        else
-            return false;
+        return inputString.matches(".*\\d.*");
     }
 
     public boolean hasOneSpecialCharacter(String inputString) {
-        if (inputString.matches(".*[!@#$%^&*].*"))
-            return true;
-        else
-            return false;
+        return inputString.matches(".*[!@#$%^&*].*");
     }
 
     public boolean isLongEnough(String inputString) {
-        if (inputString.length() >= 8)
-            return true;
-        else
-            return false;
+        return inputString.length() >= 8;
     }
 
     public boolean doesNotContainConsecutiveCharacters(String inputString) {
-        if (!inputString.matches("(?i)(?:([a-z0-9])\\1{2,})*"))
-            return true;
-        else
-            return false;
+        return !inputString.matches("(?i)(?:([a-z0-9])\\1{2,})*");
     }
 
     public int passwordCriteria(String inputString) {
-        ArrayList<Integer> passwordStrength = new ArrayList<Integer>();
+        ArrayList<Integer> passwordStrength = new ArrayList<>();
         int strength = 0;
 
         passwordStrength.add(hasOneUppercase(inputString) ? 1 : 0);
