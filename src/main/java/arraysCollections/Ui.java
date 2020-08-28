6c308bdf6c357f50c0076992cd13ed71bfc413fa
Alexandros Kalaitzidis <alexkalaitzidis2@gmail.com>
2020-08-28 15:03:10 +0300

    Implement stack example

--- src/main/java/arraysCollections/Ui.java
+++ src/main/java/arraysCollections/Ui.java
@@ -3,21 +3,28 @@ package arraysCollections;
 import arraysCollections.services.*;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Ui {
     public Ui() {
     }
 
+
     public void run() {
+        Stack exampleStack = new Stack();
+        CustomStackWithMethods customStackWithMethods = new CustomStackWithMethods();
+        customStackWithMethods.initializeStack(exampleStack);
 
         do {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Welcome to arrays and collections exercise. Please choose which program you'd like to run.");
+            System.out.println("Welcome to arrays and collections exercise. " +
+                    "Please choose which program you'd like to run.");
             System.out.println("1. List of prime numbers from another list");
             System.out.println("2. List of duplicates in a list");
             System.out.println("3. List sorted in descending order");
             System.out.println("4. String reverse using stack");
             System.out.println("5. Symmetric word checker");
+            System.out.println("6. Stack Example");
             int choice = Integer.parseInt(scanner.nextLine());
             switch (choice) {
                 case 1:
@@ -34,6 +41,9 @@ public class Ui {
                     break;
                 case 5:
                     inverseStringCheck();
+                    break;
+                case 6:
+                    stackImplementation(exampleStack);
                     break;
                 default:
                     System.out.println("Invalid Input");
@@ -64,12 +74,45 @@ public class Ui {
         StackStringReverse stackStringReverse = new StackStringReverse();
         System.out.println("Please insert a string of your choice.");
         String initialString = scanner.nextLine();
-        System.out.println("You wrote " + initialString + " and the reverse of that is " + stackStringReverse.reverse(initialString));
+        System.out.println("You wrote " + initialString + " and the reverse of that is "
+                + stackStringReverse.reverse(initialString));
     }
 
     private void inverseStringCheck() {
         SymmetricWordChecker symmetricWordChecker = new SymmetricWordChecker();
         System.out.println(symmetricWordChecker.input());
+    }
+
+    private void stackImplementation(Stack exampleStack) {
+        CustomStackWithMethods customStackWithMethods = new CustomStackWithMethods();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to stack example. I will initialize the example stack with 10 random integers." +
+                "\nYou may select which out of 3 main functions you want to run.");
+        System.out.println("Current selections are:");
+        System.out.println("1: Push to stack.");
+        System.out.println("2: Pop from stack.");
+        System.out.println("3: Peek at stack.");
+        System.out.println("4: Show the entire stack.");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                customStackWithMethods.pushToStack(exampleStack);
+                break;
+            case 2:
+                customStackWithMethods.popFromStack(exampleStack);
+                break;
+            case 3:
+                customStackWithMethods.peekAtStack(exampleStack);
+                break;
+            case 4:
+                System.out.println(exampleStack);
+                break;
+            default:
+                System.out.println("Invalid input");
+                break;
+        }
     }
 
     //Method that prompts user to rerun the program
--- src/main/java/arraysCollections/services/CustomStackWithMethods.java
+++ src/main/java/arraysCollections/services/CustomStackWithMethods.java
@@ -1,4 +1,41 @@
 package arraysCollections.services;
 
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class CustomStackWithMethods {
+    Random random = new Random();
+
+    public Stack initializeStack(Stack stack) {
+        for(int i = 0; i < 10; i++) {
+            stack.push(random.nextInt(10));
+        }
+
+        return stack;
+    }
+
+    public Stack pushToStack(Stack stack) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Provide an integer to push to stack");
+        int integerToPush = scanner.nextInt();
+        stack.push(integerToPush);
+        System.out.println("You pushed " + integerToPush + " to the example stack. The stack now is\n" + stack);
+
+        return stack;
+    }
+
+    public Stack popFromStack(Stack stack) {
+        System.out.println("The stack.pop() returns the element on the top of the stack, removing it in the process" +
+                " See it for yourself\n " + stack.pop() + "\n" + stack);
+
+        return stack;
+    }
+
+    public Stack peekAtStack(Stack stack) {
+        System.out.println("The stack.peek() returns the element on the top of the stack, but does not remove it." +
+                "See it for yourself. \n" + stack.peek() + "\n" + stack);
+
+        return stack;
+    }
 }
