0e9163580fbf2c9d9ac8ad5feb14bdd9287ab062
Alexandros Kalaitzidis <alexkalaitzidis2@gmail.com>
2020-08-30 13:29:48 +0300

    Refactor UI

--- src/main/java/arraysCollections/Ui.java
+++ src/main/java/arraysCollections/Ui.java
@@ -6,32 +6,37 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Ui {
+    Stack exampleStack = new Stack();
+    Scanner scanner = new Scanner(System.in);
+    PrimeNumberCheck primeNumberCheck = new PrimeNumberCheck();
+    DuplicateFind duplicateFind = new DuplicateFind();
+    SortInDescOrder sortInDescOrder = new SortInDescOrder();
+    StackStringReverse stackStringReverse = new StackStringReverse();
+    SymmetricWordChecker symmetricWordChecker = new SymmetricWordChecker();
+    CustomStackWithMethods customStackWithMethods = new CustomStackWithMethods();
+
+
     public Ui() {
     }
 
-
     public void run() {
-        Stack exampleStack = new Stack();
-        CustomStackWithMethods customStackWithMethods = new CustomStackWithMethods();
         customStackWithMethods.initializeStack(exampleStack);
-
         do {
-            Scanner scanner = new Scanner(System.in);
             System.out.println("Welcome to arrays and collections exercise. " +
-                    "Please choose which program you'd like to run.");
-            System.out.println("1. List of prime numbers from another list");
-            System.out.println("2. List of duplicates in a list");
-            System.out.println("3. List sorted in descending order");
-            System.out.println("4. String reverse using stack");
-            System.out.println("5. Symmetric word checker");
-            System.out.println("6. Stack Example");
+                    "Please choose which program you'd like to run.\n" +
+                    "1. List of prime numbers from another list\n" +
+                    "2. List of duplicates in a list\n" +
+                    "3. List sorted in descending order\n" +
+                    "4. String reverse using stack" +
+                    "5. Symmetric word checker\n" +
+                    "6. Stack Example");
             int choice = Integer.parseInt(scanner.nextLine());
             switch (choice) {
                 case 1:
                     primeNumber();
                     break;
                 case 2:
-                    findDuplicatelist();
+                    findDuplicateList();
                     break;
                 case 3:
                     descSortList();
@@ -50,28 +55,21 @@ public class Ui {
                     break;
             }
         } while (reRun());
-
-
     }
 
     private void primeNumber() {
-        PrimeNumberCheck primeNumberCheck = new PrimeNumberCheck();
-        System.out.println(primeNumberCheck.find());
+        System.out.println(this.primeNumberCheck.find());
     }
 
-    private void findDuplicatelist() {
-        DuplicateFind duplicateFind = new DuplicateFind();
+    private void findDuplicateList() {
         System.out.println(duplicateFind.getDuplicates());
     }
 
     private void descSortList() {
-        SortInDescOrder sortInDescOrder = new SortInDescOrder();
         System.out.println(sortInDescOrder.sorter());
     }
 
     private void stringRevWithStack() {
-        Scanner scanner = new Scanner(System.in);
-        StackStringReverse stackStringReverse = new StackStringReverse();
         System.out.println("Please insert a string of your choice.");
         String initialString = scanner.nextLine();
         System.out.println("You wrote " + initialString + " and the reverse of that is "
@@ -79,21 +77,17 @@ public class Ui {
     }
 
     private void inverseStringCheck() {
-        SymmetricWordChecker symmetricWordChecker = new SymmetricWordChecker();
         System.out.println(symmetricWordChecker.input());
     }
 
     private void stackImplementation(Stack exampleStack) {
-        CustomStackWithMethods customStackWithMethods = new CustomStackWithMethods();
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("Welcome to stack example. I will initialize the example stack with 10 random integers." +
-                "\nYou may select which out of 3 main functions you want to run.");
-        System.out.println("Current selections are:");
-        System.out.println("1: Push to stack.");
-        System.out.println("2: Pop from stack.");
-        System.out.println("3: Peek at stack.");
-        System.out.println("4: Show the entire stack.");
+                "\nYou may select which out of 3 main functions you want to run.\n" +
+                "Current selections are:\n" +
+                "1: Push to stack.\n" +
+                "2: Pop from stack.\n" +
+                "3: Peek at stack.\n" +
+                "4: Show the entire stack.");
         int choice = scanner.nextInt();
 
         switch (choice) {
@@ -118,8 +112,8 @@ public class Ui {
     //Method that prompts user to rerun the program
     private boolean reRun() {
         System.out.println("Do you want to re-run the program? (Y/n)");
-        Scanner scanner = new Scanner(System.in);
         String response = scanner.nextLine();
+
         return !response.equals("n");
     }
 }
